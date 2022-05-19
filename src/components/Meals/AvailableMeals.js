bfe78b8c09fdff3094779c23b5387090e3f6f6f4
Your Name <babaloladav@gmail.com>
2022-05-19 12:46:59 +0100

    handling the loading and http error state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -10,11 +10,17 @@ const AvailableMeals = () => {
 
   const [meals, setMeals] =  useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
 
     const fetchMeals = async () => {
       const response = await fetch('https://react-customhooks-b7061-default-rtdb.firebaseio.com/meals.json');
+
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -32,14 +38,25 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
-   
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+      
   }, []);
 
   if(isLoading){
     return <section className={classes.mealsLoading}>
       <p> Loading...</p>
     </section>
+  }
+
+  if (httpError) {
+    return(
+    <section className={classes.mealsError}>
+     <p>{httpError}</p> 
+    </section>
+    );
   }
 
     const mealsList = meals.map(meal =>( 
